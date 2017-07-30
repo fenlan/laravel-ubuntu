9b5a3398f02cd894943c007717952fd47d0f793b
fenlan <1475307818@qq.com>
2017-07-30 12:46:17 +0800

    laravel-demo

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// test 路由
+Route::get('basic1', function () {
+    return 'Hello World';
+});
+
+// test　路由参数
+Route::get('user/{id}', function ($id) {
+    return 'User-id-' . $id;
+});
+
+// test
+Route::get('user/{name?}', function ($name = 'fenlan') {
+    return 'User-name-' . $name;
+})->where('name', '[A-Za-z]+');
+
+// test 路由群组
+Route::group(['prefix' => 'member'], function () {
+    Route::get('user/member-center', ['as' => 'center', function () {
+        return route('center');
+    }]);
+    Route::any('multy1', function () {
+        return 'member-multy1';
+    });
+});
